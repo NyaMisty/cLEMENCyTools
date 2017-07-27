59a1e28e2a94aced53ed88363d2f952a440c15a4
gyc990326 <gyc990326@gmail.com>
2017-07-28 06:46:22 +0800

    support mac and linux

--- clemency_loader.py
+++ clemency_loader.py
@@ -2,8 +2,8 @@ from idaapi import *
 from idc import *
 from bitarray import bitarray
 
-#import pydevd
-#pydevd.settrace('localhost', port=15306, stdoutToServer=True, stderrToServer=True,suspend=False,overwrite_prev_trace=True,patch_multiprocessing=True)
+import pydevd
+pydevd.settrace('localhost', port=15306, stdoutToServer=True, stderrToServer=True,suspend=False,overwrite_prev_trace=True,patch_multiprocessing=True)
 
 def accept_file(li,n):
     if n:
@@ -29,7 +29,13 @@ def load_file(li, neflags, format):
     li.seek(0,SEEK_SET)
     retBuf = li.read(fileLen)
     convertedBuf = convent_bit(retBuf)
-    _mod = ctypes.WinDLL("ida.wll")
+    idaname = "ida64" if __EA64__ else "ida"
+    if sys.platform == "win32":
+        _mod = ctypes.windll[idaname + ".wll"]
+    elif sys.platform == "linux2":
+        _mod = ctypes.cdll["lib" + idaname + ".so"]
+    elif sys.platform == "darwin":
+        _mod = ctypes.cdll["lib" + idaname + ".dylib"]
     create_bytearray_linput = _mod.create_bytearray_linput
     create_bytearray_linput.argtypes = (POINTER(c_ubyte), ctypes.c_int)
     create_bytearray_linput.restype = ctypes.c_int
@@ -39,7 +45,6 @@ def load_file(li, neflags, format):
     file2base.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int)
     file2base.restype = ctypes.c_int
     file2base(retlinput,0,0,int(fileLen * 8 / 9),FILEREG_PATCHABLE)
-    file2base(retlinput, 0, 0, int(fileLen * 8 / 9), FILEREG_PATCHABLE)
     add_segm(0, 0, int(fileLen * 8 / 9), "FIRMWARE", "CODE")
     #close_linput(retlinput)
     return 1
