d72cd9095acbdfc8ba4b9154b047e2e6bd427386
gyc990326 <gyc990326@gmail.com>
2017-07-28 06:21:09 +0800

    loader to expand the 9-bit to 16-bit

--- clemency_loader.py
+++ clemency_loader.py
@@ -1,6 +1,10 @@
 from idaapi import *
 from idc import *
 from bitarray import bitarray
+import ctypes
+
+import pydevd
+pydevd.settrace('localhost', port=15306, stdoutToServer=True, stderrToServer=True,suspend=False,overwrite_prev_trace=True,patch_multiprocessing=True)
 
 def accept_file(li,n):
     if n:
@@ -17,7 +21,6 @@ def convent_bit(data):
     for i in range(len(bit_buffer)/9):
         a = bitarray("0"*7) + bit_buffer[i*9:i*9+9]
         buf += a.tobytes()[1] + a.tobytes()[0]
-
     return buf
 
 def load_file(li, neflags, format):
@@ -25,6 +28,6 @@ def load_file(li, neflags, format):
     fileLen = li.tell()
     retBuf = li.read(fileLen)
     convertedBuf = convent_bit(retBuf)
-    retlinput =  create_bytearray_linput(convertedBuf,int(fileLen * 8 / 9 * 16 / 8))
+    retlinput = create_bytearray_linput(cast(convertedBuf, POINTER(c_ubyte)),int(fileLen * 8 / 9 * 16 / 8))
     li.file2base(retlinput,0,0,len(convertedBuf),FILEREG_PATCHABLE)
     close_linput(retlinput)
