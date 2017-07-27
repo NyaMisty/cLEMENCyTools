7e3793ff560155365c7b0e52dc3a6d2ae1acd0be
gyc990326 <gyc990326@gmail.com>
2017-07-28 06:26:51 +0800

    loader to expand the 9-bit to 16-bit

--- clemency_loader.py
+++ clemency_loader.py
@@ -39,5 +39,6 @@ def load_file(li, neflags, format):
     file2base.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int)
     file2base.restype = ctypes.c_int
     file2base(retlinput,0,0,int(fileLen * 8 / 9),FILEREG_PATCHABLE)
+    add_segm(0, 0, "_start", 1)
     #close_linput(retlinput)
     return 1
