41a98437b118159b8398cfa31c349f61f5b78032
Fangrui Song <i@maskray.me>
2017-07-27 15:47:30 -0700

    Remove bitarray

--- clemency_loader.py
+++ clemency_loader.py
@@ -1,6 +1,5 @@
 from idaapi import *
 from idc import *
-from bitarray import bitarray
 
 import pydevd
 pydevd.settrace('localhost', port=15306, stdoutToServer=True, stderrToServer=True,suspend=False,overwrite_prev_trace=True,patch_multiprocessing=True)
@@ -11,17 +10,26 @@ def accept_file(li,n):
     li.seek(0)
     return "Simple cLEMENCy Loader"
 
-def convent_bit(data):
-    buf = ''
-    bit_buffer = bitarray()
-    bit_buffer.frombytes(data)
-
-    bit_buffer += bitarray('0'*(9-len(bit_buffer)%9))
-    for i in range(len(bit_buffer)/9):
-        a = bitarray("0"*7) + bit_buffer[i*9:i*9+9]
-        buf += a.tobytes()[1] + a.tobytes()[0]
-
-    return buf
+def convert_bit(data):
+    ret = bytearray()
+    x = 0
+    n = 0
+    for i in data:
+        while n >= 9:
+            t = x >> n-9
+            ret.append(t & 255)
+            ret.append(t >> 8)
+            x &= (1 << n-9) - 1
+            n -= 9
+        x = x << 8 | ord(i)
+        n += 8
+    while n >= 9:
+        t = x >> n-9
+        ret.append(t & 255)
+        ret.append(t >> 8)
+        x &= (1 << n-9) - 1
+        n -= 9
+    return str(ret)
 
 def load_file(li, neflags, format):
     li.seek(0,SEEK_END)
