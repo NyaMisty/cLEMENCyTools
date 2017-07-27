30e9b7537b0d319ddfa0d3cabdd51e45d5c6aa2c
gyc990326 <gyc990326@gmail.com>
2017-07-28 06:22:55 +0800

    loader to expand the 9-bit to 16-bit

--- clemency_loader.py
+++ clemency_loader.py
@@ -1,10 +1,9 @@
 from idaapi import *
 from idc import *
 from bitarray import bitarray
-import ctypes
 
-import pydevd
-pydevd.settrace('localhost', port=15306, stdoutToServer=True, stderrToServer=True,suspend=False,overwrite_prev_trace=True,patch_multiprocessing=True)
+#import pydevd
+#pydevd.settrace('localhost', port=15306, stdoutToServer=True, stderrToServer=True,suspend=False,overwrite_prev_trace=True,patch_multiprocessing=True)
 
 def accept_file(li,n):
     if n:
@@ -21,13 +20,24 @@ def convent_bit(data):
     for i in range(len(bit_buffer)/9):
         a = bitarray("0"*7) + bit_buffer[i*9:i*9+9]
         buf += a.tobytes()[1] + a.tobytes()[0]
+
     return buf
 
 def load_file(li, neflags, format):
-    li.seek(0,SEEK_SET)
+    li.seek(0,SEEK_END)
     fileLen = li.tell()
+    li.seek(0,SEEK_SET)
     retBuf = li.read(fileLen)
     convertedBuf = convent_bit(retBuf)
-    retlinput = create_bytearray_linput(cast(convertedBuf, POINTER(c_ubyte)),int(fileLen * 8 / 9 * 16 / 8))
-    li.file2base(retlinput,0,0,len(convertedBuf),FILEREG_PATCHABLE)
-    close_linput(retlinput)
+    _mod = ctypes.WinDLL("ida.wll")
+    create_bytearray_linput = _mod.create_bytearray_linput
+    create_bytearray_linput.argtypes = (POINTER(c_ubyte), ctypes.c_int)
+    create_bytearray_linput.restype = ctypes.c_int
+    bufPointer = cast(convertedBuf, POINTER(c_ubyte))
+    retlinput = create_bytearray_linput(bufPointer,int(fileLen * 8 / 9 * 16 / 8))
+    file2base = _mod.file2base
+    file2base.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int)
+    file2base.restype = ctypes.c_int
+    file2base(retlinput,0,0,int(fileLen * 8 / 9),FILEREG_PATCHABLE)
+    #close_linput(retlinput)
+    return 1
